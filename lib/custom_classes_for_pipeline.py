5bcdf53626625ba4dea6ae1e5b02052807b2d13d
elena_soboleva <elenasoboleva2003@yandex.ru>
2024-08-29 23:29:26 +0300

    k8s

---
+++ lib/custom_classes_for_pipeline.py
@@ -0,0 +1,22 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+
+class MakeFeatures(BaseEstimator, TransformerMixin):
+
+    def __init__(self, max_lag):
+        self.max_lag = max_lag
+
+    def transform(self, X):
+        X['dayofweek'] = X.index.dayofweek
+        X['hour'] = X.index.hour
+
+        for lag in range(1, self.max_lag + 1):
+            X['lag_{}'.format(lag)] = X['num_orders'].shift(lag)
+
+        X['rolling_mean'] = X['num_orders'].shift().rolling(self.max_lag).mean()
+
+        X = X.drop('num_orders', axis=1)
+
+        return X
+
+    def fit(self, X, y=None):
+        return self
